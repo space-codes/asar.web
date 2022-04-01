e1034fee312f796343151d5cda0251b41f3eb3f1
Mohamed Abdelrahman <mohamedabdelrahman972@gmail.com>
2022-04-01 10:46:41 +0200

    Base phosc and remove network select

--- static/js/classify.js
+++ static/js/classify.js
@@ -69,22 +69,6 @@ function downloadTheResult() {
     }
 }
 
-// Set the type of classification
-function setNetwork() {
-
-    var networkType = document.getElementById("networkSelect").value
-    if (networkType != "none") {
-        $.ajax({
-            type: "POST",
-            url: "/setNetwork/",
-            data: networkType,
-            success: function(response) {
-                console.log(response);
-            }
-        });
-    }
-}
-
 // Make a prediction
 function getPrediction() {
 
@@ -103,14 +87,8 @@ function getPrediction() {
 }
 // Make a predciton
 function predict() {
-    var networkType = document.getElementById("networkSelect").value;
-    if (networkType != "none") {
-        document.getElementById("loader").style.display = "block";
-        setNetwork();
-        getPrediction();
-    } else {
-        window.alert("Select a network type first.");
-    }
+    document.getElementById("loader").style.display = "block";
+    getPrediction();
 }
 // Save an image
 function saveTheImage() {
